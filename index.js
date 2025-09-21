75195de158d2ae4b9696fb1f367a2dc47a1ec665
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-09-21 15:39:14 -0400

    Add initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Initialize OpenAI
+import OpenAI from 'openai';
+
+// Initialize Pinecone
+import { Pinecone } from '@pinecone-database/pinecone';
+
