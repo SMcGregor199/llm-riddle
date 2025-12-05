7671ed9d799f56f847a8cada741dd1d64d51a21d
SMcGregor199 <shaynemcgregor1@gmail.com>
2025-12-05 17:23:10 -0500

    feat: add node and express to llm riddle

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+
 import dotenv from 'dotenv';
 dotenv.config();
-
 // Initialize OpenAI
 import OpenAI from 'openai';
+const openaiKey = process.env.OPENAI_API_KEY; 
+const client = new OpenAI({ apiKey: openaiKey });
 
 // Initialize Pinecone
 import { Pinecone } from '@pinecone-database/pinecone';
+const pineconeKey = process.env.PINECONE_API_KEY;
+const pinecone = new Pinecone({ apiKey: pineconeKey });
+
+
+const response = await client.responses.create({
+    model:"gpt-5",
+    input: "Write a one-sentence bedtime story about a unicorn."
+});
+
+console.log(response.output_text);
+
+
+
 
